47650a6e1801682814387caf0b129b54935506d0
nick.yang <nick.yang@yingzt.com>
2019-06-17 19:31:14 +0800

    添加frontend目录

---
+++ frontend/src/views/general/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div />
+</template>
+
+<script>
+import { test } from '@/api/general'
+export default {
+  name: 'Index',
+  data() {
+    return {
+      info: ''
+    }
+  },
+  created() {
+    this.getInfo()
+  },
+  methods: {
+    getInfo: function() {
+      test()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
